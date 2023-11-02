f8fc09761bebb87a67685523c8686283db785d43
blumj808 <146663030+blumj808@users.noreply.github.com>
2023-11-02 20:24:34 +0000

    save

---
+++ app/controllers/random_controller.rb
@@ -0,0 +1,13 @@
+class RandomController < ApplicationController
+  def randomnew
+    render({ :template => "game_templates/random_new" })
+  end
+  def random_results
+    @the_min = params.fetch("user_min").to_f
+
+    @the_max = params.fetch("user_max").to_f
+
+    @the_result = rand(@the_min..@the_max)
+    render({ :template => "game_templates/random_results" })
+  end
+end
